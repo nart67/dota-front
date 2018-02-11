16241a90ab25218369ee81efe14e2b3e5d9c665d
Dennis Tran <nartdensin@gmail.com>
2018-02-11 02:58:18 -0800

    installed Angular Materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,25 @@ import { SearchComponent } from './search/search.component';
 import { GamesComponent } from './games/games.component';
 import { SearchService } from './search.service';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    GamesComponent 
+    GamesComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule
   ],
   providers: [
     SearchService,
